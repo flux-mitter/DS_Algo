fa64adf0a35774fe55fb05dec3e3a5ed6481e3b2
preetammitra <preetammitra@localhost>
2023-01-08 19:06:28 +0530

    main commit

--- src/main/java/july2022/Test.java
+++ src/main/java/july2022/Test.java
@@ -5,7 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Coding");
+		System.out.println("Main1");
+		System.out.println("Main2");
 		System.out.println("Main3");
+		System.out.println("Main4");
+		System.out.println("Main5");
 		
 	}
 
